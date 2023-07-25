303ebb4dcda95272bfcecff4155b4f967fe92b8e
MaximHelio <gasiorowicz10@gmail.com>
2023-07-25 17:43:10 +0900

    Add crawling function

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ async function main() {
   try {
     // 1. 로그인 페이지 열기
     await driver.get('https://katalkenglish.com/login#');
-		await driver.sleep(1000);
-    // 2. ID/PW 입력
-    await driver.findElement(By.css('.ng-tns-c5-0')).sendKeys('gasiorowicz10@gmail.com');
+		await driver.sleep(2000);
+        // 2. ID/PW 입력
+    await driver.findElement(By.css('#mat-input-0')).sendKeys('gasiorowicz10@gmail.com');
 	await driver.sleep(1000);
-    await driver.findElement(By.css('.ng-tns-c5-1')).sendKeys('duruaal@123');
+    await driver.findElement(By.css('#mat-input-1')).sendKeys('duruaal@123', Key.ENTER);
     // 엔터
-    actions.sendKeys(firstStr).sendKeys(Key.ENTER).sendKeys(Key.ENTER).perform();
+    await driver.findElement(By.css('data-button')).click;
+
 	await driver.sleep(100);
     await driver.get('https://katalkenglish.com/mypage#');
-
 	await driver.sleep(1000);
-    const imgtextstock = await driver.findElement(By.css('#imgtextstock'));
-
 
     console.log("끝");
 
